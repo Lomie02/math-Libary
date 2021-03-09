ae7c76415ed9f3c5610d0a1fcc59376271a0ac84
Dylan Smith <dylansmith1844@gmail.com>
2021-03-09 15:21:27 +1100

    Adding Vector 3      V= V + V + V

---
+++ Vector3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Vector3
+{
+	public struct Vector3
+	{
+		public float x;
+		public float y;
+		public float z;
+
+		//-----------------------------
+		// Constructor
+		//-----------------------------
+
+		public Vector3(float x = 0.0f, float y = 0.0f, float z = 0.0f)
+        {
+			this.x = x;
+			this.y = y;
+			this.z = z;
+        }
+
+		public static Vector3 operator +(Vector3 x, Vector3 y, Vector3 z)
+        {
+			Vector3 results;
+
+			results.x = x.x + y.x + z.z;
+			results.y = x.y + y.y + z.y;
+			results.z = x.z + y.z + z.z;
+
+			return results;
+        }
+	}
+}
