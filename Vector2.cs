56963857b0d1053e15233c3fd5c364c8e5a1bea3
Dylan Smith <dylansmith1844@gmail.com>
2021-03-09 11:52:23 +1100

    9/03/2021 - 11:52AM

--- Vector2.cs
+++ Vector2.cs
@@ -7,24 +7,7 @@ namespace Math_Libary
         public float x;
         public float y;
 
-        //
-        public Vector2(float x = 0.0f, float y = 0.0f)
-        {
-            this.x = x;
-            this.y = y;
-
-        }
-
-        public static Vector2 operator +(Vector2 lhs, Vector2 rhs)
-        {
-            Vector2 results;
-            results.x = lhs.x + rhs.x;
-            results.y = lhs.y + rhs.y;
-
-            return results;
-
-        }
     }
 
-
+    
 }
